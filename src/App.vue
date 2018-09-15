bc357ecbd4d2b6e70573231d3cf61ae202306e50
remzov <remzovnik@gmail.com>
2018-09-15 12:58:12 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+    <div id="app">
+        <div>{{ title }}</div>
+        <div class="uk-flex uk-align-center uk-width-1-3 uk-margin-large">
+            <input class="track-input uk-input" placeholder="Введите название трека" v-on:keyup.13="addTrack">
+            <button class="uk-button uk-button-secondary" type="button" v-on:click="addTrack"
+            >Добавить ещё</button>
+        </div>
+        <app-list :tracks="tracks"></app-list>
+    </div>
+</template>
+
+<script>
+import List from './List.vue';
+export default {
+    name: 'app',
+    data () {
+        return {
+            title: 'TODO List',
+            tracks: [
+                {
+                    id: 1,
+                    text: 'Nothing Else Matters'
+                },
+                {
+                    id: 2,
+                    text: 'Love is blindness'
+                },
+                {
+                    id: 3,
+                    text: 'Oh, Children'
+                }
+            ]
+        }
+    },
+    methods: {
+        addTrack() {
+            const input = document.querySelector('.track-input');
+            const text = input.value;
+            const id = this.tracks.length + 1;
+            this.tracks.push({id, text});
+            input.value = '';
+        }
+    },
+    components: {
+       'app-list': List
+    },
+}
+ 
+</script>
+
+<style lang="scss" scoped>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    button {
+        white-space: nowrap;
+    }
+
+</style>
---
+++ src/List.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+     <ul class="uk-list">
+        <li v-for="track in tracks" v-bind:key="track.id">
+        {{ track.text }}
+        </li> 
+    </ul> 
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['tracks']
+}
+
+</script>
+
+<style>
+</style>
